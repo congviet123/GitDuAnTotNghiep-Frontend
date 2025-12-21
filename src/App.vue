da438c593595e39dad5203e6e3634b757fea4a90
congviet123 <nguyencongviet121103@gmail.com>
2025-12-21 17:00:39 +0700

    push frontend duantotnghiep

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="container mt-5">
+    <h1 class="text-primary">Test Bootstrap</h1>
+    <button class="btn btn-success">
+      <i class="bi bi-check-circle-fill"></i> Nút bấm thành công
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.mount('#app')
